d8a1e97cff68c570379c48f456d380f15ffb3d60
Rohit Kumar <rk116704@gmail.com>
2024-02-07 17:11:55 +0530

    modified

--- src/main/java/com/shoping/flipkart/security/CustomUserDetails.java
+++ src/main/java/com/shoping/flipkart/security/CustomUserDetails.java
@@ -4,7 +4,6 @@ import java.util.Collection;
 import java.util.Collections;
 
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import com.shoping.flipkart.entity.User;
@@ -18,42 +17,41 @@ public CustomUserDetails(User user) {
 	@Override
 	public Collection<? extends GrantedAuthority> getAuthorities() {
 		
-		return Collections.singleton(new SimpleGrantedAuthority(user.getUserRole().name()));
+		return Collections.singleton(null);
 	}
 
 	@Override
 	public String getPassword() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		return user.getPassword();
 	}
 
 	@Override
 	public String getUsername() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		return user.getUsername();
 	}
 
 	@Override
 	public boolean isAccountNonExpired() {
-		// TODO Auto-generated method stub
+		
 		return true;
 	}
 
 	@Override
 	public boolean isAccountNonLocked() {
-		// TODO Auto-generated method stub
 		return true;
 	}
 
 	@Override
 	public boolean isCredentialsNonExpired() {
-		// TODO Auto-generated method stub
+		
 		return true;
 	}
 
 	@Override
 	public boolean isEnabled() {
-		// TODO Auto-generated method stub
+		
 		return true;
 	}
 
--- src/main/java/com/shoping/flipkart/security/CustomUserDetailsService.java
+++ src/main/java/com/shoping/flipkart/security/CustomUserDetailsService.java
@@ -5,13 +5,11 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-
 import com.shoping.flipkart.Repository.UserRepo;
 import com.shoping.flipkart.entity.User;
-import lombok.AllArgsConstructor;
 @Service
-@AllArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService{
+	@Autowired
     private UserRepo userRepo;
     private User user;
     @Override
