c5be85d5270b9de7b5cc34c2b3ed9ab08a2ac16c
Rohit Kumar <rk116704@gmail.com>
2024-02-07 15:50:52 +0530

    Registration implemented

--- src/main/java/com/shoping/flipkart/security/SecurityConfig.java
+++ src/main/java/com/shoping/flipkart/security/SecurityConfig.java
@@ -1,7 +1,6 @@
 package com.shoping.flipkart.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityFilterAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
@@ -13,7 +12,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
-import lombok.AllArgsConstructor;
 
 @Configuration
 @EnableMethodSecurity
