fa5c7d85613414ee40bedb47bb8a1254f7b45f6e
Rohit Kumar <rk116704@gmail.com>
2024-02-08 15:25:32 +0530

    added gitignore

--- src/main/java/com/shoping/flipkart/serviceImpl/AuthServiceImpl.java
+++ src/main/java/com/shoping/flipkart/serviceImpl/AuthServiceImpl.java
@@ -1,35 +1,40 @@
 package com.shoping.flipkart.serviceImpl;
 
-import java.time.LocalDateTime;
-import java.util.Optional;
+
 import java.util.Random;
-
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import com.shoping.flipkart.Repository.CustomerRepo;
 import com.shoping.flipkart.Repository.SellerRepo;
 import com.shoping.flipkart.Repository.UserRepo;
+import com.shoping.flipkart.cacahe.CacheStore;
 import com.shoping.flipkart.entity.Customer;
 import com.shoping.flipkart.entity.Seller;
 import com.shoping.flipkart.entity.User;
+import com.shoping.flipkart.request.OtpModel;
 import com.shoping.flipkart.request.UserRequest;
 import com.shoping.flipkart.response.UserResponse;
 import com.shoping.flipkart.service.AuthService;
+import com.shoping.flipkart.utility.MessageStructure;
 import com.shoping.flipkart.utility.ResponseStructure;
 
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 @Service
 @AllArgsConstructor
 public class AuthServiceImpl implements AuthService{
-	
+	private JavaMailSender javaMailSender;
 	private SellerRepo sellerRepo;
 	private CustomerRepo customerRepo;
 	private UserRepo userRepo;
 	private ResponseStructure<UserResponse> structure;
+	private CacheStore<String> otpCacheStore;
+	private CacheStore<User> userCacheStore;
 	
 	public <T extends User>T mapToUserRequest(UserRequest userRequest){		
 		User user=null;
@@ -81,23 +86,54 @@ public class AuthServiceImpl implements AuthService{
 	
 	@Override
 	public ResponseEntity<ResponseStructure<UserResponse>> registerUser(UserRequest userRequest) {
+
+		if(userRepo.existsByEmail(userRequest.getEmail()))
+	 	    throw new RuntimeException("User is already present with the given email id");
 		
-	    User user=userRepo.findByUsername(userRequest.getEmail().split("@")[0]).map(u->{
-	    	if(u.isEmailVerified())
-	    		throw new RuntimeException("User is already verified !");
-	    	else
-	    	{
-	    		//send an email to the client with otp
-	    	}
-	    	return u;
-	    }).orElse(saveUser(userRequest));	   
-	 	    
-		UserResponse userResponse=mapToUserResponse(user);   
+		String OTP=generateOTP();
+		
+		User user=mapToUserRequest(userRequest);		
+		UserResponse userResponse=mapToUserResponse(user);
+		
+		userCacheStore.add(userRequest.getEmail(), user);
+		otpCacheStore.add(userRequest.getEmail(), OTP);		
+		
 	    structure.setStatus(HttpStatus.CREATED.value());
-		structure.setMessage("User registerd Successfully");
+		structure.setMessage("User registerd Successfully "+OTP);
 		structure.setData(userResponse);
 		
 		return new  ResponseEntity<ResponseStructure<UserResponse>>(structure,HttpStatus.CREATED);
 	}
+
+
+
+	@Override
+	public ResponseEntity<String> verifyOTP(OtpModel otpModel) {
+		User user=userCacheStore.get(otpModel.getEmail());
+		String otp=otpCacheStore.get(otpModel.getEmail());
+		
+		if(otp==null) throw new RuntimeException("OTP expired");
+		if(user==null) throw new RuntimeException("Registration session expired");
+		if(!otp.equals(otpModel.getOtp())) throw new RuntimeException("invalid otp");
+		
+		user.setEmailVerified(true);
+		userRepo.save(user);
+		return new ResponseEntity<String>("Registration done successfully ",HttpStatus.CREATED);		
+	}
+	@Async
+	private void sendMail(MessageStructure message) throws MessagingException{
+		MimeMessage mimeMessage=javaMailSender.createMimeMessage();
+		MimeMessageHelper helper=new MimeMessageHelper(mimeMessage,true);
+		helper.setSubject(message.getSubject());
+		helper.setSentDate(message.getSentDate());
+		helper.setText(message.getText());
+		javaMailSender.send(mimeMessage);
+	}
+	
+	
+	private  String generateOTP() {
+	    return String.valueOf(new Random().nextInt(100000,999999));
+	}
+	
 		
 }
